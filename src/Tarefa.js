b74dbbae097cbe4ef3ba379c19d9b6882da8cc4d
Lucia Freire <luciarigoni@live.com>
2023-08-17 17:39:35 -0300

    App de tarefas em React Native

---
+++ src/Tarefa.js
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import { FontAwesome } from '@expo/vector-icons'
+
+function Tarefa({ data, deleteItem }) {
+    return(
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={deleteItem}>
+                <FontAwesome name="trash" size={20} color="#22272e"/>
+            </TouchableOpacity>
+            <Text>{data.item}</Text>
+        </View>
+    )
+}
+
+export default Tarefa;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'rgba(196,196,196, 0.20)',
+        marginTop: 12,
+        padding: 12,
+        borderRadius: 4,
+        flexDirection: 'row'
+
+    },
+    button: {
+        right: 6,
+        bottom: 1,
+        padding: 12,
+        position: 'absolute',
+    }
+})
